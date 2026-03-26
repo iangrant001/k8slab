b40fa2ab6b77635453bfc9d637deec4b6c1a19a1
iangrant001 <ian.grant@veeam.com>
2026-03-26 11:00:50 +0000

    Update Docker installation command in setup script
    Removed 'nfs-kernel-server' from the installation command.

--- minikube-docker-setup.sh
+++ minikube-docker-setup.sh
@@ -11,7 +11,7 @@ sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install nfs-kernel-server vim htop docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get install vim htop docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 
 ### Installing Kube tools and Helm.
