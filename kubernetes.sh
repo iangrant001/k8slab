2aa42d22033d20cf05649bd28fc5b0e929a1f174
iangrant001 <ian.grant@veeam.com>
2026-03-27 09:33:21 +0000

    Add script to install Kubectl, MiniKube, and Helm
    This script installs Kubectl, MiniKube, and Helm on a Linux system.

---
+++ kubernetes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Installing Kubetools.
+
+echo "(tput setaf 10) Installing Kubectl, MiniKube and Helm."
+
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+	
+sudo apt-get install curl gpg apt-transport-https --yes
+curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
